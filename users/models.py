500817b3e5a7ca7439d19562193c5a4ae427e01d
Alain Igban <apigban@gmail.com>
2018-10-31 19:48:30 +0800

    Added Feature: Automatic Creation of Default Portfolio on User Registration

--- users/models.py
+++ users/models.py
@@ -3,17 +3,23 @@ from django.contrib.auth.models import User
 from PIL import Image
 from trading.models import Portfolio
 from django.core.validators import MaxValueValidator, MinValueValidator
+from django.utils import timezone
+from datetime import date
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpeg', upload_to='profile_pics')
     bio = models.TextField(max_length=500, blank=True)
-    location = models.CharField(max_length=30, blnk=True)
+    location = models.CharField(max_length=30, blank=True)
     birth_date = models.DateField(null=True, blank=True)
     cash = models.PositiveIntegerField(default=25000, validators=[MinValueValidator(1), MaxValueValidator(10000000)])
-    #stock_folio = models.ForeignKey(Portfolio, on_delete=models.CASCADE, blank=True, null=True)    #   a profile can have 1 portfolio
-    stock_folio = models.ManyToManyField(Portfolio, through='ProfilePortfolioJunction')             #   #   a profile can have multiple portfolio
+    # stock_folio = models.ForeignKey(Portfolio, on_delete=models.CASCADE, blank=True, null=True)    #   a profile can have 1 portfolio
+    stock_folio = models.ManyToManyField(Portfolio,
+                                         through='ProfilePortfolioJunction')  # #   a profile can have multiple portfolio
+
+
+
 
     def __str__(self):
         return f'{self.user.username} Profile'
@@ -41,4 +47,4 @@ class ProfilePortfolioJunction(models.Model):
         if not self.id:
             self.created = timezone.now()
         self.modified = timezone.now()
-        return super(StockPortfolioJunction, self).save(*args, **kwargs)
+        return super(ProfilePortfolioJunction, self).save(*args, **kwargs)
