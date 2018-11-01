e5d3718a8b7bf4f230ca13aff40ffe523f5e525b
Alain Igban <apigban@gmail.com>
2018-11-01 13:01:38 +0800

    Deleted lines for debugging

--- trading/forms.py
+++ trading/forms.py
@@ -4,33 +4,29 @@ from .validators import alphanumeric
 
 
 class StockSearchForm(forms.Form):
+    stock_search = forms.CharField(max_length=15, required=False, label='')
 
-    stock_search = forms.CharField(max_length=15 ,required=False, label='')
-
-    #company = forms.CharField(max_length=20, required=False)
+    # company = forms.CharField(max_length=20, required=False)
     class Meta:
         model = Stock
         fields = ['symbol', 'company']
 
+
 class StockPicker_dropdown(forms.Form):
     stock_list = Stock.objects.only('symbol')
 
+
 class StockPicker_multiselect(forms.Form):
-    #OPTIONS = tuple(Stock.objects.only('symbol'))
     stock_list = forms.ModelMultipleChoiceField(label='Stock', queryset=Stock.objects.only('symbol'))
-    #stock_list = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple,
-    #                                     choices=OPTIONS)
-    #choice = forms.ModelChoiceField(queryset=MyChoices.Objects.all())
 
 
 class StockPicker_name(forms.ModelForm):
     folio_name = forms.CharField(max_length=20,
                                  label='',
                                  validators=[alphanumeric],
-                                 widget=forms.TextInput(attrs={'placeholder': 'Enter Portfolio Name Here'
-
-                                 }))
+                                 widget=forms.TextInput(attrs={'placeholder': 'Enter Portfolio Name Here'}))
 
     class Meta:
         model = Portfolio
         fields = ['folio_name']
+
